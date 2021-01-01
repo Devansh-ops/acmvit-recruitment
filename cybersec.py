6b897e1a2838b84fda755e82ed6b572a5362926a
Devansh-ops <72489302+Devansh-ops@users.noreply.github.com>
2021-01-01 22:46:18 +0530

    Update cybersec.py

--- cybersec.py
+++ cybersec.py
@@ -1,14 +1,16 @@
 encrypted = "ndgzixzgvtnozvydgtvbvdinooczrjjyviynojizjacdggcjpnzviyrcvozqzmrvgfzyoczmzrvgfzyvgjiz"
 
-alp = list('abcdefghijklmnopqrstuvwxyz')
+alp = list('abcdefghijklmnopqrstuvwxyz') # list of alphabets
 
 dict1 = {}
 for key in range(26):
     for i in range(26):
-        dict1[alp[i]] = alp[(i-key)%26]
-    decrypted = ''
-    for char in encrypted:
+        dict1[alp[i]] = alp[(i-key)%26] # shift backwards by key values. if the
+                                        # number becomes negative, it counts back from 'z'
+                                        # (cuz using % 26)
+    decrypted = '' # empty string
+    for char in encrypted: # add the shifted character for a character in encrypted text
         decrypted += dict1[char]
-    #if 'the' in decrypted or 'and' in decrypted:
+    #if 'the' in decrypted or 'and' in decrypted:     # can be used to check if the decrypted text has frequently used english words
         #print("Plain text for key",key,"is",decrypted)
     print("Plain text for key",key,"is",decrypted)
