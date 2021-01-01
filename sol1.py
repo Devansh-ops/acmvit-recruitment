05df324e57576a5b1c9e6b6dfae1495c26eab654
Devansh-ops <72489302+Devansh-ops@users.noreply.github.com>
2021-01-01 22:08:53 +0530

    final soluton

--- sol1.py
+++ sol1.py
@@ -6,13 +6,9 @@ dict1 = {}
 for key in range(26):
     for i in range(26):
         dict1[alp[i]] = alp[(i-key)%26]
-    #decrypted = []
     decrypted = ''
     for char in encrypted:
-    #    decrypted.append(dict1[char])
         decrypted += dict1[char]
-    #decrypted = ''.join(decrypted)
     #if 'the' in decrypted or 'and' in decrypted:
         #print("Plain text for key",key,"is",decrypted)
     print("Plain text for key",key,"is",decrypted)
-    
